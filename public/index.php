6f706164efcefd14ef2f9404b1a3386e178c2eb6
0xRayan <rayan.seghrouchni@jobtrek.ch>
2024-02-06 11:01:19 +0100

    add: Can now edit articles

--- public/index.php
+++ public/index.php
@@ -16,32 +16,26 @@ $databasePath = __DIR__ . '/../database.db';
 $pdo = new PDO("sqlite:$databasePath");
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// Define routes
 $app->get('/', function ($request, $response) use ($twig) {
     return $twig->render($response, 'index.twig');
 });
 
 $app->post('/submit-article', function ($request, $response) use ($pdo) {
-    // Retrieve form data
     $title = $request->getParsedBody()['article_title'];
     $text = $request->getParsedBody()['article_text'];
 
-    // Insert data into the 'articles' table
     $stmt = $pdo->prepare("INSERT INTO articles (title, text) VALUES (?, ?)");
     $stmt->execute([$title, $text]);
 
-    // Redirect to the '/articles' route to display the list of articles
     return $response
         ->withHeader('Location', '/articles')
-        ->withStatus(302); // 302 Found (temporary redirect)
+        ->withStatus(302);
 });
 
 $app->get('/articles', function ($request, $response) use ($twig, $pdo) {
-    // Fetch all articles from the 'articles' table
     $stmt = $pdo->query("SELECT * FROM articles");
     $articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    // Render the 'articles.twig' template with the list of articles
     return $twig->render($response, 'articles.twig', ['articles' => $articles]);
 });
 
@@ -50,6 +44,27 @@ $app->post('/delete-article', function ($request, $response) use ($pdo) {
 
     $stmt = $pdo->prepare("DELETE FROM articles WHERE id = ?");
     $stmt->execute([$articleId]);
+
+    return $response->withHeader('Location', '/articles')->withStatus(302);
+});
+
+$app->post('/edit-article', function ($request, $response) use ($pdo, $twig) {
+    $articleId = $request->getParsedBody()['article_id'];
+
+    $stmt = $pdo->prepare("SELECT * FROM articles WHERE id = ?");
+    $stmt->execute([$articleId]);
+    $article = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    return $twig->render($response, 'edit-article.twig', ['article' => $article]);
+});
+
+$app->post('/update-article', function ($request, $response) use ($pdo) {
+    $articleId = $request->getParsedBody()['article_id'];
+    $title = $request->getParsedBody()['article_title'];
+    $text = $request->getParsedBody()['article_text'];
+
+    $stmt = $pdo->prepare("UPDATE articles SET title = ?, text = ? WHERE id = ?");
+    $stmt->execute([$title, $text, $articleId]);
 
     return $response->withHeader('Location', '/articles')->withStatus(302);
 });
